75f01e5a615b147e6a595fdb778b3a65fff7b226
Benson Maruchu <benmaruchu@gmail.com>
2018-01-03 11:43:35 +0300

    initialize repository

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @module majifix service group
+ * @version 0.1.0
+ * @description majifix service group library
+ * @author Benson Maruchu <benmaruchu@gmail.com>
+ * @public
+ */
+
+
+module.exports = function (/*options*/) {
+
+
+};
