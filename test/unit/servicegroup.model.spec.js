c5e2f7b89bae80b1207ebae6379d9e087e1a90a9
rijkerd <richardaggrey7@gmail.com>
2019-06-05 07:38:53 +0300

    refactor(unit): implement to es6

--- test/unit/servicegroup.model.spec.js
+++ test/unit/servicegroup.model.spec.js
@@ -1,20 +1,11 @@
-'use strict';
-
-
 /* dependencies */
-const path = require('path');
-const { expect } = require('chai');
-
+import { expect } from 'chai';
 
 /* declarations */
-const ServiceGroup =
-  require(path.join(__dirname, '..', '..', 'lib', 'servicegroup.model'));
-
+import ServiceGroup from '../../src/servicegroup.model';
 
 describe('ServiceGroup', () => {
-
   describe('Statics', () => {
-
     it('should expose model name as constant', () => {
       expect(ServiceGroup.MODEL_NAME).to.exist;
       expect(ServiceGroup.MODEL_NAME).to.be.equal('ServiceGroup');
@@ -22,18 +13,15 @@ describe('ServiceGroup', () => {
 
     it('should expose autopulate as options', () => {
       expect(ServiceGroup.OPTION_AUTOPOPULATE).to.exist;
-      expect(ServiceGroup.OPTION_AUTOPOPULATE)
-        .to.be.eql({
-          select: { code: 1, name: 1, color: 1 },
-          maxDepth: 1
-        });
+      expect(ServiceGroup.OPTION_AUTOPOPULATE).to.be.eql({
+        select: { code: 1, name: 1, color: 1 },
+        maxDepth: 1,
+      });
     });
 
     it('should expose default locale `en` when not set', () => {
       expect(ServiceGroup.DEFAULT_LOCALE).to.exist;
       expect(ServiceGroup.DEFAULT_LOCALE).to.equal('en');
     });
-
   });
-
 });
