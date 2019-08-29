2f9244e6bb8fbe84f0d6086aa63d30dc2e1b3f18
rijkerd <richardaggrey7@gmail.com>
2019-08-29 20:57:50 +0300

    test(unit): rename test case

--- test/unit/servicegroup.model.spec.js
+++ test/unit/servicegroup.model.spec.js
@@ -1,53 +1,51 @@
 import { expect } from '@lykmapipo/mongoose-test-helpers';
 import ServiceGroup from '../../src/servicegroup.model';
 
-describe('ServiceGroup', () => {
-  describe('Instance', () => {
-    it('`preValidate` should be a function', () => {
-      const group = ServiceGroup.fake();
-      expect(group.preValidate).to.exist;
-      expect(group.preValidate).to.be.a('function');
-      expect(group.preValidate.length).to.be.equal(1);
-      expect(group.preValidate.name).to.be.equal('preValidate');
+describe('ServiceGroup Instance', () => {
+  it('`preValidate` should be a function', () => {
+    const group = ServiceGroup.fake();
+    expect(group.preValidate).to.exist;
+    expect(group.preValidate).to.be.a('function');
+    expect(group.preValidate.length).to.be.equal(1);
+    expect(group.preValidate.name).to.be.equal('preValidate');
+  });
+
+  it('should be able to ensure color and code', done => {
+    const group = ServiceGroup.fake();
+    group.set({ color: undefined, code: undefined });
+
+    expect(group.color).to.not.exist;
+    expect(group.code).to.not.exist;
+    group.preValidate(error => {
+      expect(group.code).to.exist;
+      expect(group.code).to.eql(group.code.toUpperCase());
+      expect(group.color).to.exist;
+
+      done(error);
     });
+  });
+});
 
-    it('should be able to ensure color and code', done => {
-      const group = ServiceGroup.fake();
-      group.set({ color: undefined, code: undefined });
+describe('ServiceGroup Statics', () => {
+  it('should expose model name as constant', () => {
+    expect(ServiceGroup.MODEL_NAME).to.exist;
+    expect(ServiceGroup.MODEL_NAME).to.be.equal('ServiceGroup');
+  });
 
-      expect(group.color).to.not.exist;
-      expect(group.code).to.not.exist;
-      group.preValidate(error => {
-        expect(group.code).to.exist;
-        expect(group.code).to.eql(group.code.toUpperCase());
-        expect(group.color).to.exist;
-
-        done(error);
-      });
+  it('should expose autopulate as options', () => {
+    expect(ServiceGroup.OPTION_AUTOPOPULATE).to.exist;
+    expect(ServiceGroup.OPTION_AUTOPOPULATE).to.be.eql({
+      select: { code: 1, name: 1, color: 1 },
+      maxDepth: 1,
     });
   });
 
-  describe('Statics', () => {
-    it('should expose model name as constant', () => {
-      expect(ServiceGroup.MODEL_NAME).to.exist;
-      expect(ServiceGroup.MODEL_NAME).to.be.equal('ServiceGroup');
-    });
-
-    it('should expose autopulate as options', () => {
-      expect(ServiceGroup.OPTION_AUTOPOPULATE).to.exist;
-      expect(ServiceGroup.OPTION_AUTOPOPULATE).to.be.eql({
-        select: { code: 1, name: 1, color: 1 },
-        maxDepth: 1,
-      });
-    });
-
-    it('should expose field select option', () => {
-      expect(ServiceGroup.OPTION_SELECT).to.exist;
-      expect(ServiceGroup.OPTION_SELECT).to.be.eql({
-        code: 1,
-        name: 1,
-        color: 1,
-      });
+  it('should expose field select option', () => {
+    expect(ServiceGroup.OPTION_SELECT).to.exist;
+    expect(ServiceGroup.OPTION_SELECT).to.be.eql({
+      code: 1,
+      name: 1,
+      color: 1,
     });
   });
 });
