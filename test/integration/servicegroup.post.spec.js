455bde771c87042a03d6fcd33f74b0efe70984be
rijkerd <richardaggrey7@gmail.com>
2019-08-29 20:54:43 +0300

    test(integration): improve tests

--- test/integration/servicegroup.post.spec.js
+++ test/integration/servicegroup.post.spec.js
@@ -1,57 +1,55 @@
 import { Jurisdiction } from '@codetanzania/majifix-jurisdiction';
 import { create, clear, expect } from '@lykmapipo/mongoose-test-helpers';
-import { ServiceGroup } from '../../src';
+import { ServiceGroup } from '../../src/index';
 
-describe('ServiceGroup', () => {
+describe('ServiceGroup static post', () => {
   const jurisdiction = Jurisdiction.fake();
 
   before(done => clear(Jurisdiction, ServiceGroup, done));
 
   before(done => create(jurisdiction, done));
 
-  describe('static post', () => {
-    let servicegroup;
+  let servicegroup;
 
-    it('should be able to post', done => {
-      servicegroup = ServiceGroup.fake();
-      servicegroup.jurisdiction = jurisdiction;
+  it('should be able to post', done => {
+    servicegroup = ServiceGroup.fake();
+    servicegroup.set({ jurisdiction });
 
-      ServiceGroup.post(servicegroup, (error, created) => {
-        expect(error).to.not.exist;
-        expect(created).to.exist;
-        expect(created._id).to.eql(servicegroup._id);
-        expect(created.name.en).to.eql(servicegroup.name.en);
-        expect(created.code).to.eql(servicegroup.code);
+    ServiceGroup.post(servicegroup, (error, created) => {
+      expect(error).to.not.exist;
+      expect(created).to.exist;
+      expect(created._id).to.eql(servicegroup._id);
+      expect(created.name.en).to.eql(servicegroup.name.en);
+      expect(created.code).to.eql(servicegroup.code);
 
-        // assert jurisdiction
-        expect(created.jurisdiction).to.exist;
-        expect(created.jurisdiction.code).to.eql(
-          servicegroup.jurisdiction.code
-        );
-        expect(created.jurisdiction.name).to.eql(
-          servicegroup.jurisdiction.name
-        );
-        done(error, created);
-      });
+      // assert jurisdiction
+      expect(created.jurisdiction).to.exist;
+      expect(created.jurisdiction.code).to.eql(servicegroup.jurisdiction.code);
+      expect(created.jurisdiction.name).to.eql(servicegroup.jurisdiction.name);
+      done(error, created);
     });
   });
 
-  describe('instance post', () => {
-    let servicegroup;
+  after(done => clear(Jurisdiction, ServiceGroup, done));
+});
 
-    it('should be able to post', done => {
-      servicegroup = ServiceGroup.fake();
+describe('ServiceGroup instance post', () => {
+  before(done => clear(done));
 
-      servicegroup.post((error, created) => {
-        expect(error).to.not.exist;
-        expect(created).to.exist;
-        expect(created._id).to.eql(servicegroup._id);
-        expect(created.name).to.eql(servicegroup.name);
-        expect(created.code).to.eql(servicegroup.code);
-        done(error, created);
-      });
+  let servicegroup;
+
+  it('should be able to post', done => {
+    servicegroup = ServiceGroup.fake();
+
+    servicegroup.post((error, created) => {
+      expect(error).to.not.exist;
+      expect(created).to.exist;
+      expect(created._id).to.eql(servicegroup._id);
+      expect(created.name).to.eql(servicegroup.name);
+      expect(created.code).to.eql(servicegroup.code);
+      done(error, created);
     });
   });
 
-  after(done => clear('Jurisdiction', 'ServiceGroup', done));
+  after(done => clear(done));
 });
